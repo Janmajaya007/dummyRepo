9c65d83e75b931a8a8ba8970395b5444713c8f76
janmajaya007 <janmajaya2000@gmail.com>
2023-08-11 19:57:15 +0530

    New file added

---
+++ Dummy/Example.java
@@ -0,0 +1,11 @@
+package Dummy;
+
+/**
+ * @author Janmajaya
+ * @project Dummy_Git
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println(" Janmajaya");
+    }
+}
--- Movie_Booking_Application/src/main/java/com/janmajaya/Movie_Booking_Application/exception/MovieNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.janmajaya.Movie_Booking_Application.exception;
-
-public class MovieNotFoundException extends Exception{
-    public MovieNotFoundException(String message) {
-        super(message);
-    }
-
-}
